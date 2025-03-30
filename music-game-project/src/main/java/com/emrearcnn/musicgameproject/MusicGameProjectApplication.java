79f58c46d1fe314ecf0479ef17a8bc425cf4cd93
Mehmet Emre Arıcan <emrearcnn@outlook.com>
2025-03-30 05:43:35 +0300

    Initial project structure with microservices

---
+++ music-game-project/src/main/java/com/emrearcnn/musicgameproject/MusicGameProjectApplication.java
@@ -0,0 +1,13 @@
+package com.emrearcnn.musicgameproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicGameProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicGameProjectApplication.class, args);
+	}
+
+}
---
+++ music-game-project/src/test/java/com/emrearcnn/musicgameproject/MusicGameProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emrearcnn.musicgameproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicGameProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
